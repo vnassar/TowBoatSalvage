b4be8bc12eb93c01e4d2abf8250f4467fe020b60
vnassar <68416597+vnassar@users.noreply.github.com>
2025-06-15 15:27:59 -0400

    Updates

--- TowBoatSalvage/TowBoatSalvageWebApp/obj/Debug/net9.0/TowBoatSalvageWebApp.AssemblyInfo.cs
+++ TowBoatSalvage/TowBoatSalvageWebApp/obj/Debug/net9.0/TowBoatSalvageWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TowBoatSalvageWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cb02e6bd745519725039c2446856e256ebc43d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e7b2428c184bd7aaad9d095c8c4dff8b834de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TowBoatSalvageWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TowBoatSalvageWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
